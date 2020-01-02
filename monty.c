345d68bb16a7c6e60f0943945153d927830562db
alejolo311 <alejolo311@gmail.com>
2020-01-02 14:04:13 +0000

    create the header file and de entry point

---
+++ monty.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+/**
+ * main - entry point
+ * @argc: count of arguments
+ * @argv: The arguments
+ * Description: entry point for the program to exec monty code
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+    (void) argc;
+    (void) argv;
+	return (EXIT_SUCCESS);
+}
