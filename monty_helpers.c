2e405ef4b2671f72247d563a7d8ce97c465cc744
hb4y <hugobaymon@gmail.com>
2020-01-04 16:21:09 +0000

    style fix

--- monty_helpers.c
+++ monty_helpers.c
@@ -26,7 +26,7 @@ void open_and_read(char *f)
 	ssize_t r;
 	unsigned int ln = 1;
 	int value;
-	char *op,*val, *opcode;
+	char *op, *val, *opcode;
 
 	settings.file = fopen(f, "r");
 	while ((r = getline(&settings.line, &l, settings.file)) != -1)
@@ -44,8 +44,11 @@ void open_and_read(char *f)
 			if (is_number(val) && val != NULL)
 			{
 				value = atoi(val);
-				!settings.queue ? push_stack(&settings.stack, value) : push_queue(&settings.stack, value);
-			} 
+				if (!settings.queue)
+					push_stack(&settings.stack, value);
+				else
+					push_queue(&settings.stack, value);
+			}
 			else
 				error_handler(opcode, -129, ln);
 		} else
@@ -93,7 +96,10 @@ void exec_monty(stack_t **stack, char *opcode, int ln)
 		}
 	error_handler(opcode, -128, ln);
 }
-
+/**
+ * set - set initial values
+ * Return: void
+ */
 void set(void)
 {
 	settings.file = NULL;
@@ -101,7 +107,10 @@ void set(void)
 	settings.stack = NULL;
 	settings.queue = false;
 }
-
+/**
+ * clean - clean men
+ * Return: void
+ */
 void clean(void)
 {
 	fclose(settings.file);
